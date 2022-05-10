fa8f8e6e750631daaa291402df9d14c38e690b94
Hasini <hasinisama99@gmail.com>
2022-05-10 12:16:49 +0530

    Added data using Firestore

--- app/src/main/java/com/kln/android/androidfirebase/FirestoreFragment.kt
+++ app/src/main/java/com/kln/android/androidfirebase/FirestoreFragment.kt
@@ -1,30 +1,46 @@
 package com.kln.android.androidfirebase
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.kln.android.androidfirebase.databinding.FragmentFirstBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.kln.android.androidfirebase.databinding.FragmentFirestoreBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class FirestoreFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentFirestoreBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    val db = Firebase.firestore
+    var user = mapOf("first" to "Hasini", "last" to "Samarathunga", "age" to 23)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentFirestoreBinding.inflate(inflater, container, false)
         return binding.root
 
     }
